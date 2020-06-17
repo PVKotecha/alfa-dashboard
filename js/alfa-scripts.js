83052466ddadb49de628bba46811c49a5faf9a7c
punitkotecha@outlook.com <punitkotecha@outlook.com>
2020-06-17 19:24:51 +0530

    [fix]MCMA, MCSA, reopreder-para [wip] fill in the blanks

--- js/alfa-scripts.js
+++ js/alfa-scripts.js
@@ -570,6 +570,46 @@ var Countdown = {
     //     $('.alfa-overlay').hide();
     //   });
 
+
+    //RE-ORDER PARA - DRAG DROP
+    var adjustment;
+
+    $(".re-order-para").sortable({
+      group: 're-order-para',
+      pullPlaceholder: false,
+    //   // animation on drop
+    //   onDrop: function  ($item, container, _super) {
+    //     var $clonedItem = $('<li/>').css({height: 0});
+    //     $item.before($clonedItem);
+    //     $clonedItem.animate({'height': $item.height()});
+    
+    //     $item.animate($clonedItem.position(), function  () {
+    //       $clonedItem.detach();
+    //       _super($item, container);
+    //     });
+    //   },
+    
+      // set $item relative to cursor position
+      onDragStart: function ($item, container, _super) {
+        var offset = $item.offset(),
+            pointer = container.rootGroup.pointer;
+    
+        adjustment = {
+          left: pointer.left - offset.left,
+          top: pointer.top - offset.top
+        };
+    
+        _super($item, container);
+      },
+      onDrag: function ($item, position) {
+        $item.css({
+          left: position.left - adjustment.left,
+          top: position.top - adjustment.top
+        });
+      }
+    });
+
+
  //WAVE
  var wavesurfer = WaveSurfer.create({
     container: '#waveform',
